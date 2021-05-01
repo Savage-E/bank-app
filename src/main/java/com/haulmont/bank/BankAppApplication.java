d1abac0631837d32944843ee18f0e95eecc418db
Savage-E <vlad.kotov1999@ya.ru>
2021-05-01 14:10:21 +0300

    change tomcat to java as embedded server

--- src/main/java/com/haulmont/bank/BankAppApplication.java
+++ src/main/java/com/haulmont/bank/BankAppApplication.java
@@ -1,13 +1,15 @@
 package com.haulmont.bank;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.server.VaadinRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankAppApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BankAppApplication.class, args);
-	}
-
+public class BankAppApplication  {
+  public static void main(String[] args) {
+    SpringApplication.run(BankAppApplication.class);
+  }
 }
